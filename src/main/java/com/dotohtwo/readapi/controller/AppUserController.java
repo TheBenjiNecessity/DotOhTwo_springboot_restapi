41100e35f6cfdaf7b6bbd2b1873cba928d78924f
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-03 15:50:16 -0500

    Fix user get method

--- src/main/java/com/dotohtwo/readapi/controller/AppUserController.java
+++ src/main/java/com/dotohtwo/readapi/controller/AppUserController.java
@@ -1,22 +1,21 @@
 package com.dotohtwo.readapi.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dotohtwo.readapi.model.AppUser;
+import com.dotohtwo.readapi.model.SecurityUser;
 import com.dotohtwo.readapi.service.AppUserService;
-
-import java.security.Principal;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -25,10 +24,15 @@ public class AppUserController {
     private AppUserService appUserService;
 
     @GetMapping
-    public Optional<AppUser> get(Principal principal, @RequestParam(required = false) String username) {
-        String name = username != null ? username : principal.getName();
+    public AppUser get(SecurityUser principal, @RequestParam(required = false) String username) {
         // TODO getting a user other than the signed in user should return less data
-        return appUserService.getUserByUserame(name);
+        String name = username != null ? username : principal.getUser().getUsername();
+
+        return appUserService.getUserByUserame(name).orElseThrow(() -> {
+            return new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "AppUser not found with given username: " + name
+            );
+        });
     }
        
     @PreAuthorize("hasAuthority('USER')")

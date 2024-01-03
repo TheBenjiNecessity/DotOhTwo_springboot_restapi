acda97fd8a37fa3c72b336ef7d736957e5852e71
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-03 15:53:37 -0500

    Code cleanup

--- src/main/java/com/dotohtwo/readapi/auth/SecurityConfig.java
+++ src/main/java/com/dotohtwo/readapi/auth/SecurityConfig.java
@@ -11,12 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.dotohtwo.readapi.detailService.JpaUserDetailsService;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -37,47 +34,18 @@ public class SecurityConfig {
     @Value("${jwtKey}")
     private byte[] jwtKey;
 
-    // @Bean
-    // UserDetailsService userDetailsService() {
-
-
-    //     return new DOTUserDetailsManager();
-    // }
-
-    // @Bean
-    // @Order(Ordered.HIGHEST_PRECEDENCE)
-    // SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-    //     OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-    //     return http.formLogin(withDefaults()).build();
-    // }
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // return http
-        //         .csrf(csrf -> csrf.disable())
-        //                  .authorizeHttpRequests(auth -> {
-        //             auth.requestMatchers("/review").permitAll();
-        //             auth.requestMatchers("/user/{id}").permitAll();
-        //             auth.requestMatchers("/favicon.ico").permitAll();
-        //             auth.anyRequest().authenticated();
-        //         })
-        //         .userDetailsService(jpaUserDetailsService)
-        //         .headers(withDefaults()) // sameOrigin?
-        //         .httpBasic(withDefaults())
-        //         .build();
-
         return http
                 .csrf(csrf -> csrf.disable()) // TODO remove for security reasons
                 .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> {
-                    //auth.requestMatchers("/review").permitAll();
-                    //auth.requestMatchers("/user/{id}").permitAll();
-                    auth.requestMatchers("/favicon.ico").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .userDetailsService(jpaUserDetailsService)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer(auth -> auth.jwt(jwt -> jwt.decoder(jwtDecoder())))
+                //.httpBasic(withDefaults())
                 .build();
     }
 

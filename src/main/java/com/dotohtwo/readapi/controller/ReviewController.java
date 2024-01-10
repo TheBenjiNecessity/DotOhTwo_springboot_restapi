f70e466576680df3086746912c641282f8b3e101
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-10 13:42:32 -0500

    Add more review endpoints

--- src/main/java/com/dotohtwo/readapi/controller/ReviewController.java
+++ src/main/java/com/dotohtwo/readapi/controller/ReviewController.java
@@ -1,6 +1,10 @@
 package com.dotohtwo.readapi.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/review")
 public class ReviewController {
-    @GetMapping("")
-    public String get(@RequestParam(value = "id") String id) {
+    @GetMapping("/{id}")
+    public String get(@PathVariable("id") String id) {
       return "review";
     }
 
@@ -18,6 +22,23 @@ public class ReviewController {
       return "list";
     }
 
+    @PostMapping("/{id}")
+    public String post(@PathVariable("id") String id) {
+      return "review";
+    }
 
+    // @PatchMapping("/{id}")
+    // public String get(@PathVariable("id") String id) {
+    //   return "review";
+    // }
 
+    @PutMapping("/{id}")
+    public String put(@PathVariable("id") String id) {
+      return "review";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") String id) {
+      return "review";
+    }
 }

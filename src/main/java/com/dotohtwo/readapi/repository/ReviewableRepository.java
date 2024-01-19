03ceb133975b20ebd30e6eb6f62d1b9013c58c96
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-19 13:09:44 -0500

    Fix query string format

--- src/main/java/com/dotohtwo/readapi/repository/ReviewableRepository.java
+++ src/main/java/com/dotohtwo/readapi/repository/ReviewableRepository.java
@@ -10,7 +10,14 @@ import java.util.Collection;
 
 public interface ReviewableRepository extends JpaRepository<Reviewable, Long> {
     @Query(
-        value = "SELECT r.* FROM reviewable r WHERE r.info -> 'title' ->> :locale LIKE CONCAT('%', :text, '%') LIMIT :limit OFFSET :offset",
+        value = """
+            SELECT r.*
+            FROM reviewable r
+            WHERE r.info -> 'title' ->> :locale
+                LIKE CONCAT('%', :text, '%')
+            LIMIT :limit
+            OFFSET :offset
+        """,
         nativeQuery = true
     )
     Collection<Reviewable> findReviewablesByTitle(@Param("text") String searchText, @Param("locale") String locale, @Param("limit") Integer limit, @Param("offset") Integer offset); // TODO performance?

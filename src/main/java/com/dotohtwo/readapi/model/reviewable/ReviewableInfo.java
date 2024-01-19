afc483ba26312a946a01e55d75d58e18fd044048
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-19 14:32:55 -0500

    Add localizable title value

--- src/main/java/com/dotohtwo/readapi/model/reviewable/ReviewableInfo.java
+++ src/main/java/com/dotohtwo/readapi/model/reviewable/ReviewableInfo.java
@@ -1,5 +1,8 @@
 package com.dotohtwo.readapi.model.reviewable;
  
 import java.io.Serializable;
+import java.util.HashMap;
 
-public class ReviewableInfo implements Serializable {}
+public class ReviewableInfo implements Serializable {
+    public HashMap<String, String> title;
+}

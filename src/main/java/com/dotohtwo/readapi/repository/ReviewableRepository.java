7a10a932452a7d682cc25b32fea4af44ab2d0ccc
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-19 12:53:56 -0500

    Add findReviewablesByTitle query

--- src/main/java/com/dotohtwo/readapi/repository/ReviewableRepository.java
+++ src/main/java/com/dotohtwo/readapi/repository/ReviewableRepository.java
@@ -1,7 +1,17 @@
 package com.dotohtwo.readapi.repository;
- 
+
 import com.dotohtwo.readapi.model.Reviewable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ReviewableRepository extends JpaRepository<Reviewable, Long> {}
+import java.util.Collection;
+
+public interface ReviewableRepository extends JpaRepository<Reviewable, Long> {
+    @Query(
+        value = "SELECT r.* FROM reviewable r WHERE r.info -> 'title' ->> :locale LIKE CONCAT('%', :text, '%') LIMIT :limit OFFSET :offset",
+        nativeQuery = true
+    )
+    Collection<Reviewable> findReviewablesByTitle(@Param("text") String searchText, @Param("locale") String locale, @Param("limit") Integer limit, @Param("offset") Integer offset); // TODO performance?
+}

5ab675a2351f658ba658bf7efd035be6103d4906
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-05-16 22:14:58 -0400

    Remove comments

--- src/main/java/com/dotohtwo/readapi/ReadapiApplication.java
+++ src/main/java/com/dotohtwo/readapi/ReadapiApplication.java
@@ -5,21 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReadapiApplication {
-	// @Autowired
-    // private JdbcTemplate jdbcTemplate;
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReadapiApplication.class, args);
 	}
- 
-    // @Override
-    // public void run(String... args) throws Exception {
-    //     String sql = "INSERT INTO students (name, email) VALUES ("
-    //             + "'Nam Ha Minh', 'nam@codejava.net')";
-         
-    //     int rows = jdbcTemplate.update(sql);
-    //     if (rows > 0) {
-    //         System.out.println("A new row has been inserted.");
-    //     }
-    // }
 }

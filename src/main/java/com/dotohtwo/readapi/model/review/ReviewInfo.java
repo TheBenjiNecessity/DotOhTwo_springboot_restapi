9ba6ff34a0818094cfecbf8ee18aa01a03141f6d
Benjamin Wishart <benjamin.wishart@gmail.com>
2026-03-03 22:01:42 -0500

    Remove review/reply entities

--- src/main/java/com/dotohtwo/readapi/model/review/ReviewInfo.java
+++
@@ -1,6 +0,0 @@
-package com.dotohtwo.readapi.model.review;
-
-import java.io.Serializable;
-
-public class ReviewInfo implements Serializable {
-}
--- src/main/java/com/dotohtwo/readapi/repository/ReviewRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dotohtwo.readapi.repository;
-
-import com.dotohtwo.readapi.repository.DAO.ReviewDAO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<ReviewDAO, Long> {}

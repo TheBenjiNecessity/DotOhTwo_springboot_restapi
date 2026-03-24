737edb1330c174648b9ab698f0f0d376913ff461
Benjamin Wishart <benjamin.wishart@gmail.com>
2026-03-23 21:47:03 -0400

    Add ability to debug locally without needing a jwt

---
+++ src/main/java/com/dotohtwo/readapi/auth/NoSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.dotohtwo.readapi.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("nosec")
+public class NoSecurityConfig {
+
+    @Bean
+    @Primary
+    SecurityFilterChain noSecFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+}
